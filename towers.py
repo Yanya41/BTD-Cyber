4239fb25c2eeea2b305a196f97eda24cdee964a6
Yanya41 <yanya2019@gmail.com>
2025-11-15 21:15:15 +0200

    mainMenu

--- towers.py
+++ towers.py
@@ -7,10 +7,16 @@ class Towers:
     class Wizard:
         def __init__(self):
             self.tower_type = "wizard"
+            self.damage = 2
+            self.attack_speed = 1.5
+            self.range = 150
 
     class Archer:
         def __init__(self):
             self.tower_type = "archer"
+            self.damage = 1
+            self.attack_speed = 1.0
+            self.range = 200
 
 
 Player().towers_add(1, "wizard")
